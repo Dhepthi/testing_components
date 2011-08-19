2aa7594837ff3de22f21ac8b83bdc8f67756b632
Dhepthi <dhepthiln@spritle.com>
2011-08-19 15:34:15 +0530

    Formatted the Display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
- def link_to_field(name,form_name)
+  def link_to_field(name,form_name)
     new_form = render(form_name)
     link_to_function(name, h("load_content_for_tabs(\"#{escape_javascript(new_form)}\")"))
   end
